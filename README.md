7c6351bbc12870a39b3aee64a27762b9787e91c0
jean2040 <jeanpaul.gomez@gmail.com>
2017-04-22 14:28:08 -0400

    Initial Commit
    Adding Srtat files for Project 3  Angular V1

---
+++ README.md
@@ -0,0 +1,16 @@
+The initial start of a Movie Gallery that lists various movie titles and metadata on the screen. Using AngularJS (>1.5), and no jQuery, create a working Movie Gallery as described on your assignment in Moodle.
+
+Make sure to track all your changes using good commit messages (http://chris.beams.io/posts/git-commit/). 
+
+For the project, you will be editing:
+/index.html
+/css/style.css
+/js/controllers/HomeController.js
+
+You will also be adding files (poster images) to:
+/img/
+
+There are example images in:
+/examples/
+
+Leave the index.php file alone. It is used to make sure your Heroku server deploys correctly with PHP functionality and also automatically redirects visitors to index.html.
---
+++ js/controllers/HomeController.js
@@ -0,0 +1,118 @@
+app.controller('HomeController', ['$scope', function($scope) {
+	
+	
+	/* THESE ARE YOUR LIST OF MOVIES AND ACCOMPANYING METADATA
+	//	
+	// 	They are in the following format:
+	// 	title (String): the name of the movie
+	// 	iscore (Number): the IMDB score
+	// 	rating (String): the movie's MPAA rating 
+	// 	released (Date): the release date
+	// 	country (String): the country of production
+	// 	posters (Array): an array of String values with the URL to movie posters
+	// 	imdb (String): the URL to the corresponding IMDB website
+	// 	website (String): the URL to the corresponding official website
+	// 	likes (Number): a fictitious number of user likes
+	// 	dislikes (Number): a fictitious number of user dislikes
+	// 	posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
+	//
+	// FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THIS LIST
+	*/
+	$scope.movies = [
+    { 
+      title:	'The Shawshank Redemption', 
+      iscore:	9.3,
+      rating:	'R',
+      runtime:	142,
+      released:	new Date('1994', '10', '14'),
+      country:	'USA',
+      posters:	['img/shawshank.jpg','img/shawshank2.jpg','img/shawshank3.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0111161',
+      website:	'https://www.facebook.com/ShawshankRedemptionFilm',
+      likes:	1021,
+      dislikes:	122,
+      posterindex: 0
+	},
+    { 
+      title:	'The Godfather', 
+      iscore:	9.2,
+      rating:	'R',
+      runtime:	175,
+      released:	new Date('1972', '03', '24'),
+      country:	'USA',
+      posters:	['img/godfather.jpg','img/godfather_2.jpg','img/godfather_3.jpg'],
+      imdb:		'[http://www.imdb.com/title/tt0068646',
+      website:	'https://www.facebook.com/thegodfather',
+      likes:	928,
+      dislikes:	109,
+      posterindex: 0
+	},
+    { 
+      title:	'The Godfather: Part II', 
+      iscore:	9.0,
+      rating:	'R',
+      runtime:	202,
+      released:	new Date('1974', '12', '20'),
+      country:	'USA',
+      posters:	['img/godfather2.jpg','img/godfather2_2.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0071562',
+      website:	'https://www.facebook.com/thegodfather',
+      likes:	855,
+      dislikes:	99,
+      posterindex: 0
+	},
+    { 
+      title:	'The Dark Knight', 
+      iscore:	8.9,
+      rating:	'PG-13',
+      runtime:	152,
+      released:	new Date('2008', '07', '18'),
+      country:	'USA',
+      posters:	['img/darkknight.jpg','img/darkknight2.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0468569',
+      website:	'http://www.42entertainment.com/work/whysoserious',
+      likes:	828,
+      dislikes:	127,
+      posterindex: 0
+	},
+    { 
+      title:	'Schindler\'s List', 
+      iscore:	8.9,
+      rating:	'R',
+      runtime:	195,
+      released:	new Date('1993', '11', '30'),
+      country:	'USA',
+      posters:	['img/schindlerslist.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0108052',
+      website:	'https://www.uphe.com/schindlers-list',
+      likes:	812,
+      dislikes:	101,
+      posterindex: 0
+	}
+	
+	
+  ];
+	
+	
+
+	
+	/* ADD VARIABLES FOR STEP 3 HERE */
+	
+	
+	
+	
+	
+	
+	
+	
+	/* ADD FUNCTIONS FOR STEP 7 HERE */
+		
+	
+	
+	
+	
+	
+	
+	
+	
+}]);
