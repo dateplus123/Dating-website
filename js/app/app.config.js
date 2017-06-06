d9bb2bcdacd4300802fdc934d8deda6fa4a83e1c
JP2040 <jeanpaul.gomez@gmail.com>
2017-06-06 00:39:09 -0400

    created service to share data between controllers

--- js/app/app.config.js
+++ js/app/app.config.js
@@ -19,3 +19,30 @@ angular.module('dateSite').
 
 
         });
+
+    
+    angular.module('dateSite').service('sharedProperties', function() {
+    //NEED TO CHANGE THIS TO WORK WITH OBJECTS INSTEAD OF SINGLE VALUES
+    var selectedUser = 'Test';
+    var selectedPicUser = '';
+    var objectValue = {
+        data: 'test object value'
+    };
+    
+    return {
+        getString: function() {
+            return selectedUser;
+        },
+        getImg: function() {
+            return selectedPicUser;
+        },
+        setUser: function(value, img) {
+            selectedUser = value;
+            selectedPicUser = img;
+           
+        },
+        getObject: function() {
+            return objectValue;
+        }
+    }
+});
